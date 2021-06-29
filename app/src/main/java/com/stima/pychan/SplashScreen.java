7d9823aaee116ada5a6987161303f5ea4febec46
ubaid-Q <qubaid5@gmail.com>
2021-06-30 00:03:56 +0500

    Updated

---
+++ app/src/main/java/com/stima/pychan/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.stima.pychan;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MessageListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
